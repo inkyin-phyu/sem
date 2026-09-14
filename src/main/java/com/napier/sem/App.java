e66c3edeac4b67f1512380daf2210f7213a49e90
Inkyin-Phyu <40841055@napier.ac.uk>
2026-09-14 16:07:09 +0630

    mongo

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,6 +25,6 @@ public class App
 
         // Check document in collection
         Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        System.out.print(myDoc.toJson());
     }
 }
